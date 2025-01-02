322eb918e173c33e4169398f6a9d2cec18e4f90d
Thiago Landi <thiagolandi0902@gmail.com>
2025-01-02 11:02:44 -0300

    Service layer, component registration

---
+++ src/main/java/com/thiagolandi/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.thiagolandi.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagolandi.course.entities.User;
+import com.thiagolandi.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}

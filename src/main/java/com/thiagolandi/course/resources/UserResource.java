c5a189f587517f68f345ee29d54ea50e879b2b82
Thiago Landi <thiagolandi0902@gmail.com>
2025-01-30 14:45:48 -0300

    User insert

--- src/main/java/com/thiagolandi/course/resources/UserResource.java
+++ src/main/java/com/thiagolandi/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.thiagolandi.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.thiagolandi.course.entities.User;
 import com.thiagolandi.course.services.UserService;
@@ -18,7 +22,8 @@ public class UserResource {
 
 	@Autowired
 	private UserService service;
-	 
+
+	//Get é usado para recuperar dados do banco de dados
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
 		List<User> list = service.findAll();
@@ -29,6 +34,15 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	//Post usa quando vai inserir algo no banco de dados
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				  .buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 }
